79e0fdb5ab47104a2cee0b4dec4da6d7b050827f
Michael Baker <devnull17@gmail.com>
2014-03-16 02:10:09 -0400

    Abstracted referee behavior out of main server script

--- server.js
+++ server.js
@@ -17,42 +17,11 @@ for (var i = 0; i < 10 ; i++) {
 }
 
 var referee = new Entity({
-    behaviors: [{
-        action: function (context) {
-
-            var count = 0;
-            var last = null;
-            for (var i = 0; i < context.length; i++) {
-                if (!context[i].dead) {
-                    last = context[i];
-                    count++;
-                }
-            }
-
-            if (count > 1) {
-//                console.log(count + ' players remain.');
-                return true;
-            }
-
-            if (count === 1) {
-                console.log(last.name + " is the winner!");
-                aq.remove(last); // prevent the last contestant from continuing to pull the trigger
-                return false;
-            }
-
-            if (count === 0) {
-                console.log("Oh shit, everybody's dead!");
-                return false;
-            }
-
-        },
-        period: 400
-    }],
-    context: players,
-    name: "Contestant #" + i
+    behaviors: [behaviors.Referee],
+    context: {
+        aq: aq,
+        players: players
+    },
+    name: "Referee" + i
 });
 aq.register(referee);
-
-//for (i = 0; i < 10 ; i++) {
-//    setTimeout(players[i].behaviors[0].action.call(players[i]), players[i].behaviors[0].period);
-//}

2e1976a77ebe14e297586bc3cf9903a8ecf24ba6
Michael Baker <devnull17@gmail.com>
2014-03-16 01:28:05 -0400

    Added basic action queue functionality.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var Entity = require('./entity');
+#!/usr/local/bin/node
+
+var Entity = require('./lib').entity;
+var ActionQueue = require('./lib').actionQueue;
+
+var aq = new ActionQueue();
 
 var players = [];
 for (var i = 0; i < 10 ; i++) {
-    players[i] = new Entity();
+    players[i] = new Entity({name: "Contestant #" + i});
+    aq.register(players[i]);
 }
+//for (i = 0; i < 10 ; i++) {
+//    setTimeout(players[i].behaviors[0].action.call(players[i]), players[i].behaviors[0].period);
+//}
